95a916b583896d3eedc50801f4321d317e6666ac
Jakub Kaláb <jakubkalab@gmail.com>
2026-07-31 09:31:21 +0200

    add Project-scoped Task updates

--- lib/taskman/tasks.ex
+++ lib/taskman/tasks.ex
@@ -12,6 +12,19 @@ defmodule Taskman.Tasks do
     |> Repo.all()
   end
 
+  def get_task_for_project(%Project{id: project_id}, id) when is_integer(id) and id > 0 do
+    Repo.get_by(Task, id: id, project_id: project_id)
+  end
+
+  def get_task_for_project(%Project{} = project, id) when is_binary(id) do
+    case Integer.parse(id) do
+      {parsed, ""} -> get_task_for_project(project, parsed)
+      _invalid -> nil
+    end
+  end
+
+  def get_task_for_project(%Project{}, _id), do: nil
+
   def create_task(%Project{id: project_id}, attrs \\ %{}) do
     %Task{project_id: project_id}
     |> Task.changeset(attrs)
@@ -21,4 +34,16 @@ defmodule Taskman.Tasks do
   def change_task(%Task{} = task, attrs \\ %{}) do
     Task.changeset(task, attrs)
   end
+
+  def update_task(
+        %Project{id: project_id},
+        %Task{project_id: project_id} = task,
+        attrs
+      ) do
+    task
+    |> Task.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def update_task(%Project{}, %Task{}, _attrs), do: {:error, :not_found}
 end
