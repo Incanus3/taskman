d61ea9506cfa85120794912a955c628a66cfe954
Jakub Kaláb <jakubkalab@gmail.com>
2026-09-02 21:37:19 +0200

    Adopt AshPostgres without changing Ecto behavior

--- lib/taskman/repo.ex
+++ lib/taskman/repo.ex
@@ -1,5 +1,9 @@
 defmodule Taskman.Repo do
-  use Ecto.Repo,
+  use AshPostgres.Repo,
     otp_app: :taskman,
-    adapter: Ecto.Adapters.Postgres
+    warn_on_missing_ash_functions?: false
+
+  def installed_extensions, do: ["uuid-ossp", "citext"]
+
+  def min_pg_version, do: %Version{major: 15, minor: 0, patch: 0}
 end
